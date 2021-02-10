c4afbdfdccb39c4e0223b2fd5cab6f170f3fe44f
athif <liteboook@gmail.com>
2021-02-10 15:52:13 +0530

    edited

--- Uniq/settings.py
+++ Uniq/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vw56p28ak7)oi@=hf5!ag@cze0fd690t*7_1wfd7=!u=c)ew6i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
